37d5d5574f1d7306a890fee1da92eabfc8077681
liumonac <liu.mona.c@gmail.com>
2018-06-21 17:36:52 -0600

    tower challenge

--- CodingChallenges/TowerOfHanoi.java
+++ CodingChallenges/TowerOfHanoi.java
@@ -5,21 +5,29 @@ Tower of Hanoi
 Description: Puzzle game with 3 rods and n disks.
 Objective: re-stack the disks in the same order on a different rod.
 
+
+example for 2 disks:
+shift first disk from rod 1 to rod 2
+shift second disk from rod 1 to rod 3
+shit first disk from rod 2 to rod 3
+
 */
 
 class TowerOfHanoi {
 
 	static void recurseTower (int n, int fromRod, int toRod, int otherRod) {
 		if (n == 1) {
-			System.out.println ("Move disk 1 from rod " + fromRod " to Rod " + toRod);
+			System.out.println ("Move disk 1 from rod " + fromRod + " to Rod " + toRod);
 			return;
 		}
 
 		recurseTower (n-1, fromRod, otherRod, toRod);
+		System.out.println ("Move disk " + n + " from rod " + fromRod + " to Rod " + toRod);
+
 	}
 
 	public static void main (String args[]) {
-		int n = 3;
+		int n = 2;
 
 		int fromRod = 1;
 		int otherRod = 2;
