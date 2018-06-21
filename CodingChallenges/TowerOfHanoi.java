ea56dbd9103884f8f343b30b7ef915db5ddc77fd
liumonac <liu.mona.c@gmail.com>
2018-06-21 17:41:14 -0600

    add args parsing

--- CodingChallenges/TowerOfHanoi.java
+++ CodingChallenges/TowerOfHanoi.java
@@ -23,15 +23,26 @@ class TowerOfHanoi {
 
 		recurseTower (n-1, fromRod, otherRod, toRod);
 		System.out.println ("Move disk " + n + " from rod " + fromRod + " to Rod " + toRod);
-
+		recurseTower (n-1, otherRod, toRod, fromRod);
 	}
 
 	public static void main (String args[]) {
-		int n = 2;
+		int n = 4;
 
 		int fromRod = 1;
 		int otherRod = 2;
 		int toRod = 3;
+
+		if (args.length == 1) {
+			n = Integer.parseInt (args[0]);
+		}
+
+		if (args.length == 4) {
+			n = Integer.parseInt (args[0]);
+			fromRod = Integer.parseInt (args[1]);
+			toRod = Integer.parseInt (args[2]);
+			otherRod = Integer.parseInt (args[3]);
+		}
 
 		recurseTower (n, fromRod, toRod, otherRod);
 	}
